171d88e29066ef393c92b05eec5424b55fe312ce
jack <jack11jack11jack@gmail.com>
2024-01-29 12:35:52 +0800

    1.Add log tracking for debug

--- app/src/main/java/io/horizontalsystems/bankwallet/core/DebugTree.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/DebugTree.kt
@@ -7,8 +7,29 @@ class DebugTree: Timber.DebugTree() {
     companion object {
         const val TAG = "[wallet]"
     }
+    override fun createStackElementTag(element: StackTraceElement): String =
+        "${super.createStackElementTag(element)}|${element.methodName}()|${element.fileName}:${element.lineNumber}"
 
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        super.log(priority, TAG + tag, message, t)
+
+        var mainTag = TAG
+        var methodName = ""
+        var msg = ""
+        var line = ""
+
+
+        tag!!.split("|").forEachIndexed() { index, s ->
+            when (index) {
+                0 -> mainTag = s
+                1 -> methodName = s
+                2 -> line = s
+            }
+        }
+        super.log(
+            priority,
+            "[${Thread.currentThread().name}]" + TAG ,
+            "$mainTag | $methodName | $line | $message",
+            t,
+        )
     }
 }
